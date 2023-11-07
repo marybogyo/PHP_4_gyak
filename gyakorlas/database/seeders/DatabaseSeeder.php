87ed26fe54c87de6753c9fd2ebaed50991b4f0be
marybogyo <bratis.marianna@gmail.com>
2023-11-07 18:42:13 +0100

    gyar feltolt

--- gyakorlas/database/seeders/DatabaseSeeder.php
+++ gyakorlas/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\User::factory(25)->create();
+        \App\Models\Book::factory(25)->create();
+        \App\Models\Copy::factory(25)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
